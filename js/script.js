bdf2fae310cf70294a21d023bfdbd305d5fd391c
default <yunus.emre.guzel@mths.ca>
2025-04-03 00:37:30 +0000

    added the templte

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Emre Guzel
+// Created on: March 31 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function volumeOfPyramid() {
+  // Setting the varibles
+  const LENGTH_Of_PYRAMID = parseFloat(document.getElementById("length-of-pyramid").value)
+  const WIDTH_OF_PYRAMID = parseFloat(document.getElementById("width-of-pyramid").value)
+  const HEIGHT_OF_PYRAMID = parseFloat(document.getElementById("height-of-pyramid").value)
+  const volumeOfPyramid = (LENGTH_Of_PYRAMID * WIDTH_OF_PYRAMID * HEIGHT_OF_PYRAMID) / 3
+  // clacualtoins 
+  document.getElementById("Volume").innerHTML = 
+  "Volume is: " + volumeOfPyramid.toFixed(2) + " mm³";
+}
